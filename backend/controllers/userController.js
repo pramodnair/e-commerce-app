1c84204b67f786e4cba0a8024b6907aa3cf9f6d0
Pramod Nair <pramod.n49@gmail.com>
2024-01-27 13:00:37 +0530

    Add login functionality with JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,61 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import { generateToken } from "../utils/generateToken.js";
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Email or password does not match");
+  }
+});
+
+export const register = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user) {
+    res.status(400);
+    throw new Error("User already exists");
+  } else {
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (newUser) {
+      generateToken(res, newUser._id);
+      res.json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  }
+});
+
+export const logout = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "User logged out successfully" });
+});
 
 export const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
